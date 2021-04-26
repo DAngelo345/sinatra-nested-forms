4f8f0facb5442951ea90da0231f5c99f9a786952
DAngelo Thomas <dangelo1thomas@gmail.com>
2021-04-25 20:02:51 -0400

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,21 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do 
+      erb :root
+    end
+
+    get '/new' do 
+      # @pirate = Pirate.new(params[:name])
+      # binding.pry
+      erb :"/pirates/new"
+    end
+    
+    post '/pirates' do
+
+
+      erb :"/pirates/show"
+    end
 
   end
 end
